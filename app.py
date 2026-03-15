484fef73004e36e61a948f50020303da03883c0f
ahmedk-gamal <ae29809151807596@depi.eui.edu.eg>
2026-03-15 16:33:41 +0200

    Initial commit: Added model and web app
    Added model and web app

---
+++ app.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+from tensorflow.keras.models import load_model
+
+# كود الويب أبلكيشن بتاعنا هنا...
+# (انسخ كود الـ Streamlit اللي بعتهولك قبل كدة وحطه هنا)
